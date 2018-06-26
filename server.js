a161f9aaaff703326133042ba2fe48b0d30ccdf0
Philippe Luchansky <philuchansky@gmail.com>
2018-06-26 12:45:22 -0700

    add mongo connection

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const
   mongoose = require('mongoose'),
   PORT = 3000
 
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+  console.log(err || "Connected to MongoDB 👍")
+})
+
 app.use(logger('dev'))
 
 app.listen(PORT, (err) => {
-  console.log(err || `Server running.`)
+  console.log(err || `Server running on port ${PORT} 🤘`)
 })
